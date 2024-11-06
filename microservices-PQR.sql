497c6a032b2780d7b931bc458c43d63844621305
Deiby Gerleyn Taborda Giraldo <gerleytaborda645@gmail.com>
2024-11-05 22:02:13 -0500

    Correcciones del script de la base de datos

--- microservices-PQR.sql
+++ microservices-PQR.sql
@@ -4,12 +4,12 @@ USE microservices_PQRS;
 
 CREATE TABLE rol(
     id_rol INT AUTO_INCREMENT PRIMARY KEY,
-    rol ENUM('CLIENTE', 'ADMINISTRADOR', 'SUPERADMINISTRADOR'),
+    rol VARCHAR(20) NOT NULL,
     date_register_rol TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status_rol ENUM('ACTIVO', 'INACTIVO') NOT NULL
+    status_rol ENUM('ACTIVO', 'INACTIVO') DEFAULT 'ACTIVO'
 );
 
-CREATE TABLE user(
+CREATE TABLE usuario(
     id_user INT AUTO_INCREMENT PRIMARY KEY,
     name_user VARCHAR(60) NOT NULL,
     lastname_user VARCHAR(60) NOT NULL,
@@ -17,8 +17,8 @@ CREATE TABLE user(
     phone_number_user VARCHAR(10) NULL,
     rol_user INT NOT NULL,
     date_register_user TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status_user ENUM('ACTIVO', 'INACTIVO') DEFAULT('ACTIVO')
-    FOREIGN KEY (rol_user) REFERENCES rol(id_rol);
+    status_user ENUM('ACTIVO', 'INACTIVO') DEFAULT 'ACTIVO',
+    FOREIGN KEY (rol_user) REFERENCES rol(id_rol)
 );
 
 CREATE TABLE pqrs(
@@ -27,6 +27,6 @@ CREATE TABLE pqrs(
     type_pqrs ENUM('PETICIÓN', 'QUEJA', 'RECLAMO', 'SUGERENCIA'),
     descripcion_pqrs TEXT NOT NULL,
     data_register_pqrs TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status_pqrs ENUM('PENDIENTE', 'EN PROCESO', 'FINALIZADO') DEFAULT('PENDIENTE'),
-    FOREIGN KEY (id_user) REFERENCES user('id_user');
+    status_pqrs ENUM('PENDIENTE', 'EN PROCESO', 'FINALIZADO') DEFAULT 'PENDIENTE',
+    FOREIGN KEY (id_user) REFERENCES user('id_user')
 );
